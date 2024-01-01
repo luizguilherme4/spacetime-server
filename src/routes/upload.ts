37d4407cc5fdb1644119d856ec6acdfc38526b4f
Luiz Guilherme <101076506+luizguilherme4@users.noreply.github.com>
2024-01-01 13:54:52 -0400

    feat: finalizando o projeto

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,6 +1,6 @@
-import { FastifyInstance } from 'fastify'
 import { randomUUID } from 'node:crypto'
 import { extname, resolve } from 'node:path'
+import { FastifyInstance } from 'fastify'
 import { createWriteStream } from 'node:fs'
 import { pipeline } from 'node:stream'
 import { promisify } from 'node:util'
@@ -11,7 +11,7 @@ export async function uploadRoutes(app: FastifyInstance) {
   app.post('/upload', async (request, reply) => {
     const upload = await request.file({
       limits: {
-        fileSize: 5_242_880, // 5mb
+        fileSize: 10_242_880, // 5mb
       },
     })
 
@@ -32,10 +32,10 @@ export async function uploadRoutes(app: FastifyInstance) {
     const fileName = fileId.concat(extension)
 
     const writeStream = createWriteStream(
-      resolve(__dirname, '../../uploads/', fileName),
+      resolve(__dirname, '..', '..', 'uploads', fileName),
     )
 
-    await pump(upload.filename, writeStream)
+    await pump(upload.file, writeStream)
 
     const fullUrl = request.protocol.concat('://').concat(request.hostname)
     const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
